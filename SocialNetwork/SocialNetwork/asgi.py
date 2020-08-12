79f1c01fdf28702784c7880de595532d2e840f5b
MrFlava <thatelitemaili33t@gmail.com>
2020-08-12 16:35:46 +0300

    Renamed Project

--- SocialNetwork/SocialNetwork/asgi.py
+++ SocialNetwork/SocialNetwork/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for MainProject project.
+ASGI config for SocialNetwork project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MainProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SocialNetwork.settings')
 
 application = get_asgi_application()
