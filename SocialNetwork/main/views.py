64ae3414247269cb1594e3c311f3f0aa67fcd10c
MrFlava <thatelitemaili33t@gmail.com>
2020-08-14 16:24:09 +0300

    Added login and logout functional

--- SocialNetwork/main/views.py
+++ SocialNetwork/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
@@ -23,3 +23,23 @@ def register(request):
 
     form = UserCreationForm
     return render(request, "main/register.html", context={"form": form})
+
+
+def logout_request(request):
+    logout(request)
+    return redirect("main:homepage")
+
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("main:homepage")
+    form = AuthenticationForm()
+    return render(request, "main/login.html", {"form": form})
+
