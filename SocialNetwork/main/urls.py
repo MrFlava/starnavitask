be71c3738619010b8cd62fafdccb060aa98d4324
MrFlava <thatelitemaili33t@gmail.com>
2020-08-22 12:44:30 +0300

    Optimized imports

--- SocialNetwork/main/urls.py
+++ SocialNetwork/main/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from django.conf import settings
-from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from . import views
 
